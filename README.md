d6702cace72a13bfe2b2957f5cf0a1ade1d4f005
G-Wang <ywa289@sfu.ca>
2018-04-29 19:10:48 -0700

    added comment for readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # char-rnn
 char-rnn practice
+
+## Usage
+Note below usage assumes cuda is enabled. Make sure to remove cuda code if running on cpu
+```python
+python main.py
+```
--- tests.py
+++ tests.py
@@ -15,12 +15,39 @@ vocab_length = len(vocab) # vocabulary length
 data_len = len(train_data) # get length of training data
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-epochs = 100000
-seq_batch_size = 100
-print_yes = 100
-loss_func = torch.nn.functional.nll_loss
 
-def main():
+
+
+def no_test_forward():
+    loss_func = torch.nn.functional.nll_loss
+    net = RNN(100, 100, 100)
+    net.to(device) # add cuda to device
+    optim = torch.optim.Adam(net.parameters(),lr=1e-4)
+    # step 2: create a training batch of data, size 101, format this data and convert it to pytorch long tensors
+    dat = get_batch(train_data,100)
+    dat = torch.LongTensor([vocab.find(item) for item in dat])
+    # step 3: convert our dat into input/output
+    x_t = dat[:-1]
+    y_t = dat[1:]
+    ho = net.init_hidden()
+    # remember to load all variables used by the model to the device, this means the i/o as well as the hidden state
+    x_t, y_t, ho = x_t.to(device), y_t.to(device), ho.to(device)
+    # test forward pass
+    log_prob, hidden = net.forward(x_t, ho)
+    # let's see if the forward pass of the next hidden state is already cuda
+    #log_prob2, hidden2 = net.forward(x_t, hidden)
+    loss = loss_func(log_prob, y_t)
+    optim.zero_grad()
+    loss.backward()
+    optim.step()
+
+
+
+def test_running():
+    epochs = 100000
+    seq_batch_size = 100
+    print_yes = 100
+    loss_func = torch.nn.functional.nll_loss
     # create network and optimizer
     net = RNN(100, 120, 150, 2)
     net.to(device) # add cuda to device
@@ -48,6 +75,3 @@ def main():
             print('\n epoch {}, loss:{} \n'.format(epoch, loss))
             # make sure to pass True flag for running on cuda
             print('sample speech:\n', run_words(net, vocab, 500, True))
-
-if __name__=="__main__":
-    main()
